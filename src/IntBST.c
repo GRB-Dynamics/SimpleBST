0021a6972d361d47eaeaabf2e0c6bb6b4d67747d
God Tariq <tariq@ramesar.net>
2016-08-22 01:52:40 -0400

    Filled in some code for some of the functions. Probably won't compile since there are likely errors.

--- src/IntBST.c
+++ src/IntBST.c
@@ -9,8 +9,13 @@
 // Our node for the tree. Contains left child, right child, and the data itself.
 struct GIntNode
 	{
+	// Shouldn't these be GIntNode's? Don't they point to other nodes?
+	/*
 	struct IntNode *Left;
 	struct IntNode *Right;
+	*/
+	struct GIntNode *Left;
+	struct GIntNode *Right;
 	int Value;
 	};
 
@@ -40,26 +45,34 @@ HIntBST IntBSTCreate(void)
 	return (HIntBST)newnode;
 	}
 
+//////////////////////////////////////////////
+// Deletes a node in a BST and calls itself to delete child nodes.
+static bool GDeleteTree(struc GIntNode *node)
+	{
+	// If the node has a left child, call GDeleteTree on it.
+	if(node.Left != 0)
+		{ GDeleteTree(node.Left); }
+
+	// If the node has a right child, call GDeleteTree on it.
+	if(node.Right != 0)
+		{ GDeleteTree(node.Right); }
+
+	// Now delete the node.
+	// Just freeing it from memory may work?
+	free(node);
+	}
 
 //////////////////////////////////////////////
 // Destroy an entire binary tree.
 bool IntBSTDestroy(HIntBST htree)
 	{
-	/* Fill In */
-	// To-do:
-	// Normally if this was C# we could just destroy the root node and garbage cleanup would destroy the unreachable children.
-	// But I'm guessing since C has memory issues we need to go through and destroy each child node?
+	// If the htree (root node) isn't null, then call GDeleteTree to recursively delete it and call GDeleteTree on it's childnodes.
 
-	/*
-		The simplist code is a recursive deletion of the tree. 
+	if(htree != 0)
+		{		
+		GDeleteTree(htree.Root); // I think "htree.Root" is what we use to pass a GIntNode? These pointers are killing me.
+		}
 
-		you should emplement a function called
-		
-		static bool GDeleteTree(struc GIntNode *node);
-
-		The static prefix is to make sure that the function is in inaccessable
-		to anyone using the function outside this file.
-	*/
 	return false;
 	}
 
@@ -70,7 +83,6 @@ bool IntBSTAdd(HIntBST htree,int val)
 	{
 	/* Fill In */
 	// Go through each node of the tree determining whether to go left or right.
-	// 3 cases...
 	return false;
 	}
 
@@ -79,16 +91,69 @@ bool IntBSTAdd(HIntBST htree,int val)
 // Search the tree for a value.
 bool IntBSTIsMember(HIntBST htree,int val)
 	{
-	/* Fill In */
+	// Start at the root node. If the value is equal, return true.
+	// If the value is less, look at the left child. If the value is greater, look at the right child. 
+	
+	// Create a temporary node to traverse the tree to look for the value.
+	struc GIntNode *tempnode;
+
+	// Start tempnode off at the root node.
+	tempnode = htree.Root;
+	
+	// While we still have a node to check (because the tree isn't empty or we didn't pass over the value)...
+	while(tempnode != 0)
+		{
+		// If the value of this node is what we want, return true and break.
+		if(val == tempnode.Value) { return true; }
+		
+		// If the value is smaller and there's a left child to this node, set tempnode to the left child for the next iteration of this while loop.
+		if(val < tempnode.Value && tempnode.Left != 0) { tempnode = tempnode.Left; }
+
+		// Same thing for right.
+		if(val < tempnode.Value && tempnode.Right != 0) { tempnode = tempnode.Right; }
+
+		}
+
 	return false;
 	}
 
 
 //////////////////////////////////////////////
+// Prints the value of a node, then calls PrintNode on it's left and right children if they exist.
+// Should work Depth-First.
+void PrintNode(struc GIntNode *node)
+	{
+	// Print the value of the current node.
+	printf("%d\n", node.Value);
+
+	// Call PrintNode on the left child, if it exists.
+	if(node.Left != 0)
+		{ PrintNode()node.Left; }
+
+	// Call PrintNode on the right child, if it exists.
+	if(node.Right != 0)
+		{ PrintNode()node.Right; }
+	}
+
+//////////////////////////////////////////////
 // Print all members of the tree.
 bool IntBSTPrint(HIntBST htree)
 	{
-	/* Fill In */
+	// If the tree is empty print that it's empty.
+	if(htree == 0) 
+		{ 
+		printf("Tree is empty."); 
+		return false;
+		}
+	
+	// Otherwise it must have members so print the value of each one.
+	else
+		{
+		// Since there aren't queue's or stacks in C...
+		// Recursively print the nodes by calling PrintNode on the root.
+		PrintNode(htree.Root);
+		}	
+	
 	return false;
 	}
 
--- src/IntBST.h
+++ src/IntBST.h
@@ -6,30 +6,11 @@
 #include <stdbool.h>
 
 /////////////////////////////////////////////////
-// A structure for our binary tree. 
-// It's a...pointer called HIntBST which is made up of GIntBST root node
-/*
-	This is subtle example of encapsulation in C.  By creating a pointer
-	to some structure, we are creating an opaque handle to the internal
-	structure of GIntBST.  Call the IntBST.h and IntBST.c a module(a unit of code)
-	that you will use in a larger project.  When we use a module, all we 
-	have to do is include the header file associated with the module(ie IntBST.h)
-	
-	Your comment that 
-		"It's a...pointer called HIntBST which is made up of GIntBST root node"
-	should not be here.  We are purposely hiding the implementation details from
-	the user of the module.  The user can only use the functions below.
-
-	This idea is sometimes called loose coupling of code.
-	
-	A better comment would be:  Create abstact handle to a binary search tree instance.
-
-	In the next commit: erase this comment
-*/
+// Create abstact handle to a binary search tree instance.
 typedef struct GIntBST *HIntBST;
 
 ////////////////////////////////////////////////
-/*  Creates a Binary Search Tree object with an initial value */
+/*  Creates a Binary Search Tree */
 HIntBST IntBSTCreate(void);
 
 

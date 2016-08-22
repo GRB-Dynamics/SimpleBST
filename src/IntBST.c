e146ecda98bd290dfcc29f3883777428830d8b24
Murali Devi <profdevi@ymail.com>
2016-08-22 10:30:47 -0400

    Fixed some pointers reference and moved around some code

--- src/IntBST.c
+++ src/IntBST.c
@@ -5,11 +5,15 @@
 #include <stdbool.h>
 #include "IntBST.h"
 
-/////////////////////////////////////////////
+
+//*******************************************************
+//* Module Elements( Internal to file)
+//*******************************************************
 // Our node for the tree. Contains left child, right child, and the data itself.
 struct GIntNode
 	{
 	// Shouldn't these be GIntNode's? Don't they point to other nodes?
+	// See the documentation pdf image
 	/*
 	struct IntNode *Left;
 	struct IntNode *Right;
@@ -21,12 +25,57 @@ struct GIntNode
 
 
 /////////////////////////////////////////////
-// A special node that serves as the root of the tree.
+// A special node that serves as a pointer to the root of the tree. (see doc image)
 struct GIntBST
 	{
 	struct GIntNode *Root;
 	};
 
+
+//////////////////////////////////////////////
+// Deletes a node in a BST and calls itself to delete child nodes.
+static bool GDeleteTree(struct GIntNode *node)
+	{
+	// If the node has a left child, call GDeleteTree on it.
+	if(node->Left != 0)
+		{ GDeleteTree(node->Left); }
+
+	// If the node has a right child, call GDeleteTree on it.
+	if(node->Right != 0)
+		{ GDeleteTree(node->Right); }
+
+	// Now delete the node.
+	// Just freeing it from memory may work?
+	free(node);
+
+	return true;
+	}
+
+
+//////////////////////////////////////////////
+// Prints the value of a node, then calls PrintNode on it's left and right children if they exist.
+// Should work Depth-First.
+/** Converted to inorder traversal **/
+static void GPrintTree(struct GIntNode *node)
+	{
+	if(node==0) { return; }
+
+	// Print Left
+	GPrintTree(node->Left);
+
+	// Print the value of the current node.
+	printf("%d ", node->Value);
+
+	// Print Right
+	GPrintTree(node->Right);
+	}
+
+
+
+
+//*******************************************************
+//* Module Interface Functions
+//*******************************************************
 
 /////////////////////////////////////////////
 // HIntBST structure that returns the parent node of a binary tree?
@@ -45,33 +94,19 @@ HIntBST IntBSTCreate(void)
 	return (HIntBST)newnode;
 	}
 
-//////////////////////////////////////////////
-// Deletes a node in a BST and calls itself to delete child nodes.
-static bool GDeleteTree(struc GIntNode *node)
-	{
-	// If the node has a left child, call GDeleteTree on it.
-	if(node.Left != 0)
-		{ GDeleteTree(node.Left); }
-
-	// If the node has a right child, call GDeleteTree on it.
-	if(node.Right != 0)
-		{ GDeleteTree(node.Right); }
-
-	// Now delete the node.
-	// Just freeing it from memory may work?
-	free(node);
-	}
 
 //////////////////////////////////////////////
 // Destroy an entire binary tree.
 bool IntBSTDestroy(HIntBST htree)
 	{
 	// If the htree (root node) isn't null, then call GDeleteTree to recursively delete it and call GDeleteTree on it's childnodes.
-
 	if(htree != 0)
 		{		
-		GDeleteTree(htree.Root); // I think "htree.Root" is what we use to pass a GIntNode? These pointers are killing me.
+		GDeleteTree(htree->Root); // I think "htree.Root" is what we use to pass a GIntNode? These pointers are killing me.
 		}
+
+	// Free the GIntBST Node
+	free(htree);
 
 	return false;
 	}
@@ -95,45 +130,27 @@ bool IntBSTIsMember(HIntBST htree,int val)
 	// If the value is less, look at the left child. If the value is greater, look at the right child. 
 	
 	// Create a temporary node to traverse the tree to look for the value.
-	struc GIntNode *tempnode;
+	struct GIntNode *tempnode;
 
 	// Start tempnode off at the root node.
-	tempnode = htree.Root;
+	tempnode = htree->Root;
 	
 	// While we still have a node to check (because the tree isn't empty or we didn't pass over the value)...
 	while(tempnode != 0)
 		{
 		// If the value of this node is what we want, return true and break.
-		if(val == tempnode.Value) { return true; }
+		if(val == tempnode->Value) { return true; }
 		
 		// If the value is smaller and there's a left child to this node, set tempnode to the left child for the next iteration of this while loop.
-		if(val < tempnode.Value && tempnode.Left != 0) { tempnode = tempnode.Left; }
-
-		// Same thing for right.
-		if(val < tempnode.Value && tempnode.Right != 0) { tempnode = tempnode.Right; }
-
+		if(val < tempnode->Value)
+			{ tempnode = tempnode->Left; }
+		else
+			{ tempnode = tempnode->Right; }
 		}
 
 	return false;
 	}
 
-
-//////////////////////////////////////////////
-// Prints the value of a node, then calls PrintNode on it's left and right children if they exist.
-// Should work Depth-First.
-void PrintNode(struc GIntNode *node)
-	{
-	// Print the value of the current node.
-	printf("%d\n", node.Value);
-
-	// Call PrintNode on the left child, if it exists.
-	if(node.Left != 0)
-		{ PrintNode()node.Left; }
-
-	// Call PrintNode on the right child, if it exists.
-	if(node.Right != 0)
-		{ PrintNode()node.Right; }
-	}
 
 //////////////////////////////////////////////
 // Print all members of the tree.
@@ -145,15 +162,19 @@ bool IntBSTPrint(HIntBST htree)
 		printf("Tree is empty."); 
 		return false;
 		}
-	
+
+	/*	
 	// Otherwise it must have members so print the value of each one.
 	else
-		{
-		// Since there aren't queue's or stacks in C...
-		// Recursively print the nodes by calling PrintNode on the root.
-		PrintNode(htree.Root);
-		}	
+
+	** No Need for else
+	*/
 	
-	return false;
+	// Since there aren't queue's or stacks in C...
+	// Recursively print the nodes by calling PrintNode on the root.
+	GPrintTree(htree->Root);
+	printf("\n");
+	
+	return true;
 	}
 

3575b572d8e9cca4eb17a5a04258b8ac86141e10
God Tariq <tariq@ramesar.net>
2016-08-23 01:19:05 -0400

    Filled in code for Adding Value to Tree. Removed some excess comments.

--- src/IntBST.c
+++ src/IntBST.c
@@ -12,12 +12,6 @@
 // Our node for the tree. Contains left child, right child, and the data itself.
 struct GIntNode
 	{
-	// Shouldn't these be GIntNode's? Don't they point to other nodes?
-	// See the documentation pdf image
-	/*
-	struct IntNode *Left;
-	struct IntNode *Right;
-	*/
 	struct GIntNode *Left;
 	struct GIntNode *Right;
 	int Value;
@@ -34,7 +28,7 @@ struct GIntBST
 
 //////////////////////////////////////////////
 // Deletes a node in a BST and calls itself to delete child nodes.
-static bool GDeleteTree(struct GIntNode *node)
+static void GDeleteTree(struct GIntNode *node)
 	{
 	// If the node has a left child, call GDeleteTree on it.
 	if(node->Left != 0)
@@ -44,17 +38,13 @@ static bool GDeleteTree(struct GIntNode *node)
 	if(node->Right != 0)
 		{ GDeleteTree(node->Right); }
 
-	// Now delete the node.
-	// Just freeing it from memory may work?
+	// Now delete the node itself.
 	free(node);
-
-	return true;
 	}
 
 
 //////////////////////////////////////////////
-// Prints the value of a node, then calls PrintNode on it's left and right children if they exist.
-// Should work Depth-First.
+// Recursively print the values in the tree.
 /** Converted to inorder traversal **/
 static void GPrintTree(struct GIntNode *node)
 	{
@@ -102,7 +92,7 @@ bool IntBSTDestroy(HIntBST htree)
 	// If the htree (root node) isn't null, then call GDeleteTree to recursively delete it and call GDeleteTree on it's childnodes.
 	if(htree != 0)
 		{		
-		GDeleteTree(htree->Root); // I think "htree.Root" is what we use to pass a GIntNode? These pointers are killing me.
+		GDeleteTree(htree->Root); 
 		}
 
 	// Free the GIntBST Node
@@ -116,8 +106,58 @@ bool IntBSTDestroy(HIntBST htree)
 // Add a node to the binary tree. 
 bool IntBSTAdd(HIntBST htree,int val)
 	{
-	/* Fill In */
-	// Go through each node of the tree determining whether to go left or right.
+	// Go through the tree checking whether each value is < or >
+		
+	struct GIntNode *tempnode; 	// Create a temporary node to traverse the tree to look for the value.
+	tempnode = htree->Root;		// Start tempnode off at the root node.
+
+	while(tempnode != 0)
+		{
+		// If value is equal then we can't add it.
+		if(val == tempnode->Value) { break; }
+		
+		// If the value is less...
+		if(val < tempnode->Value)
+			{
+			// If the left child is empty, add the node there.
+			if(tempnode->Left == 0)
+				{  
+				// Create the node.
+				struct GIntNode *newnode;
+				newnode->Value = val;
+				newnode->Left = 0;
+				newnode->Right = 0;
+
+				// Change the tempnode to point to this new node.
+				tempnode->Left = newnode;
+				}
+			// The left child isn't empty, so continue traversing the tree.
+			else
+				{ tempnode = tempnode->Left; }
+			}
+
+		// If the value is greater...
+		else
+			{
+			// If the right child is empty, add the node there.
+			if(tempnode->Right == 0)
+				{  
+				// Create the node.
+				struct GIntNode *newnode;
+				newnode->Value = val;
+				newnode->Left = 0;
+				newnode->Right = 0;
+
+				// Change the tempnode to point to this new node.
+				tempnode->Right = newnode;
+				}
+			// The right child isn't empty, so continue traversing the tree.
+			else
+				{ tempnode = tempnode->Right; }
+			}
+		}
+
+	// Why are these functions bool and not void? Return true if the function is successful, false if something goes wrong?
 	return false;
 	}
 
@@ -126,14 +166,8 @@ bool IntBSTAdd(HIntBST htree,int val)
 // Search the tree for a value.
 bool IntBSTIsMember(HIntBST htree,int val)
 	{
-	// Start at the root node. If the value is equal, return true.
-	// If the value is less, look at the left child. If the value is greater, look at the right child. 
-	
-	// Create a temporary node to traverse the tree to look for the value.
-	struct GIntNode *tempnode;
-
-	// Start tempnode off at the root node.
-	tempnode = htree->Root;
+	struct GIntNode *tempnode; 	// Create a temporary node to traverse the tree to look for the value.
+	tempnode = htree->Root;		// Start tempnode off at the root node.
 	
 	// While we still have a node to check (because the tree isn't empty or we didn't pass over the value)...
 	while(tempnode != 0)
@@ -162,15 +196,7 @@ bool IntBSTPrint(HIntBST htree)
 		printf("Tree is empty."); 
 		return false;
 		}
-
-	/*	
-	// Otherwise it must have members so print the value of each one.
-	else
-
-	** No Need for else
-	*/
 	
-	// Since there aren't queue's or stacks in C...
 	// Recursively print the nodes by calling PrintNode on the root.
 	GPrintTree(htree->Root);
 	printf("\n");
